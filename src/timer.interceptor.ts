3d1d53446ca4e44c59ddf59d5c587bc77862439a
Baiburtian Violetta <bayviol@yandex.ru>
2023-03-01 12:37:44 +0300

    feat: initial commit lab2

---
+++ src/timer.interceptor.ts
@@ -0,0 +1,19 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { map, Observable } from "rxjs";
+
+export interface Response {
+  data: string;
+}
+
+@Injectable()
+export class TransformationInterceptor<T> implements NestInterceptor<T, Response> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response> {
+    const startTime = Date.now();
+    return next
+      .handle()
+      .pipe(map((data) => ({ data: ` + ${Number(Date.now() - startTime)/1000} s (server)` })));
+  }
+}
